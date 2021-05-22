25a130091d657d618d0b62e4147ca9f1a457db12
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-05-22 10:40:29 +0700

    changes add

---
+++ src/main/java/com/nyash/app/springpractice/JazzMusic.java
@@ -0,0 +1,9 @@
+package com.nyash.app.springpractice;
+
+public class JazzMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "Dying Light";
+    }
+}
--- src/main/java/com/nyash/app/springpractice/TestSpring.java
+++ src/main/java/com/nyash/app/springpractice/TestSpring.java
@@ -8,9 +8,11 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
-        System.out.println(testBean.getName());
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
