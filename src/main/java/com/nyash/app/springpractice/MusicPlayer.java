72e917a59a97be812235f3fd6f95e0dfbb87d7a4
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-05-22 09:42:34 +0700

    changes add

--- src/main/java/com/nyash/app/springpractice/MusicPlayer.java
+++ src/main/java/com/nyash/app/springpractice/MusicPlayer.java
@@ -1,4 +1,13 @@
 package com.nyash.app.springpractice;
 
 public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing "+ music.getSong());
+    }
 }
